1801fc122326d2fd4aa4efd322d2c15a93e41094
Jesutoni <91492815+jtsenpai@users.noreply.github.com>
2023-05-02 21:44:52 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <h2 className="title">About</h2>
       <p>
         I am a frontend developer with expertise in HTML, CSS, JavaScript, and
-        React. He is passionate about creating visually appealing and responsive
+        React. I am passionate about creating visually appealing and responsive
         web interfaces.
       </p>
     </section>
